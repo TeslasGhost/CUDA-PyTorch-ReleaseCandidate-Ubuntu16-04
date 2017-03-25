8391ea8a607cd4d76d305d39c44ef33e6e8e9ce5
teslasghost <brendan@wearepopgun.com>
2017-03-25 14:01:13 -0700

    PyTorch Script

--- install-pytorch.sh
+++ install-pytorch.sh
@@ -9,8 +9,12 @@ CMAKE_PREFIX_PATH=/home/ubuntu/miniconda3/envs/cuda
 # install optional dependencies
 conda install numpy mkl setuptools cmake gcc cffi
 
-# install optional dependencies
-conda install numpy mkl setuptools cmake gcc cffi
+# lapack support
+conda install -c soumith magma-cuda80
+
+# download pytorch source
+git clone https://github.com/pytorch/pytorch.git
+cd pytorch
 
 # install python requirements
 pip install -r requirements.txt
