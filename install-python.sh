2a74fe13e9b4129f57a8e4872867c61f47c809ea
teslasghost <brendan@wearepopgun.com>
2017-03-25 13:05:14 -0700

    Python Script

---
+++ install-python.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo -e "\e[96mUpdating System"
+echo -e "\e[97m"
+sudo apt-get update
+sudo apt-get install fluidsynth
+sudo apt-get install python3 python3-dev python3-pip
+
+echo -e "\e[96mDownloading MiniConda3"
+echo -e "\e[97m"
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O conda_install.sh
+chmod +x conda_install.sh
+
+echo -e "\e[96mInstalling MiniConda3"
+echo -e "\e[97m"
+bash conda_install.sh
+
+echo -e "\e[96mCreating Conda Environment & Installing Scientific Compute Dependencies"
+echo -e "\e[97m"
+conda create -n cuda python=3.6 anaconda numpy scipy matplotlib
