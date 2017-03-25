6a90887a5b6e20c8fc2b9e9cf78e25b013c227bf
teslasghost <brendan@wearepopgun.com>
2017-03-25 10:27:33 -0700

    small changes

--- install-cuda.sh
+++ install-cuda.sh
@@ -3,11 +3,8 @@
 # Download the CUDNN Release Candidate 
 wget http://developer.download.nvidia.com/compute/redist/cudnn/v6.0/cudnn-8.0-linux-x64-v6.0-rc.tgz
 
-# Move the download and change permissions
-mv cudnn-8.0-linux-x86-v6.0-rc.tgz CUDA-PyTorch-ReleaseCandidate-Ubuntu16-04/
-
 cd CUDA-PyTorch-ReleaseCandidate-Ubuntu16-04
-sudo chmod +x cudnn-8.0.tgz
+sudo chmod +x cudnn-8.0-linux-x64-v6.0-rc.tgz
 
 # Update the system
 sudo apt-get update
@@ -46,7 +43,7 @@ sudo sh ./extracts/cuda-linux64-rel-8.0.61-21551265.run
     #   or, add /usr/local/cuda-8.0/lib64 to /etc/ld.so.conf and run ldconfig as root
 
 # Untar, copy to system, and clean up
-tar xf cudnn-8.0.tgz
+tar xf cudnn-8.0-linux-x64-v6.0-rc.tgz
 sudo cp cuda/lib64/* /usr/local/cuda/lib64/
 sudo cp cuda/include/cudnn.h /usr/local/cuda/include/
 echo -e "export CUDA_HOME=/usr/local/cuda\nexport PATH=\$PATH:\$CUDA_HOME/bin\nexport LD_LIBRARY_PATH=\$LD_LINKER_PATH:\$CUDA_HOME/lib64" | tee -a ~/.bashrc ~/.zshrc
