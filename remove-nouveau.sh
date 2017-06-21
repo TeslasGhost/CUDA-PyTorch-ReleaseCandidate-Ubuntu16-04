9e403319b26c61437fe0d2b87fd0336f5427fa20
Brendan Clohesy <brendan_460@hotmail.com>
2017-06-21 18:54:33 +1000

    added remove-nouveau script

---
+++ remove-nouveau.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo echo "blacklist nouveau" > /etc/modprobe.d/blacklist-nouveau.conf
+sudo echo "options nouveau modeset=0" >> /etc/modprobe.d/blacklist-nouveau.conf
+sudo update-initramfs -u
+
+
